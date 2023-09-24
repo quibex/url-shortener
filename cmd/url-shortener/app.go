5264319b3029a67d18b7a0d65d9282cdc18da79e
quibex <quibex@ya.ru>
2023-09-24 13:22:10 +0300

    add slog logger

--- cmd/url-shortener/app.go
+++ cmd/url-shortener/app.go
@@ -1,11 +1,42 @@
 package main
 
 import (
-	"log"
+	"log/slog"
+	"os"
 	"urlshortener/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envProd  = "prod"
+	envDev   = "dev"
 )
 
 func main() {
 	cfg := config.MustLoad()
-	log.Println(cfg)
+
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting url-shortener", slog.String("env", cfg.Env))
+	log.Debug("debug mode")
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	}
+	return log
 }
